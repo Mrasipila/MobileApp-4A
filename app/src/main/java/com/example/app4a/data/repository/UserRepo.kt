355d57bd5b4b76366390e9ac08356cf399668a7d
Meyssan Kanzari <meyssank@outlook.fr>
2020-12-26 20:04:59 +0100

    [Feature][localDB] Clean Architecture "Done"

---
+++ app/src/main/java/com/example/app4a/data/repository/UserRepo.kt
@@ -0,0 +1,9 @@
+package com.example.app4a.data.repository
+
+import com.example.app4a.domain.entities.User
+
+class UserRepo {
+    suspend fun createUser(user: User){
+
+    }
+}
---
+++ app/src/main/java/com/example/app4a/domain/entities/User.kt
@@ -0,0 +1,5 @@
+package com.example.app4a.domain.entities
+
+class User {
+    val email : String = "Please Enter Your Email Address"
+}
