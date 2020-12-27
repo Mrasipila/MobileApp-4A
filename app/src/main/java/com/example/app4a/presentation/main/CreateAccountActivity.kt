bd381a6c36d2fb3ead577986af7899b2546132b5
Meyssan Kanzari <meyssank@outlook.fr>
2020-12-28 00:24:59 +0100

    [Feature][CreateAccountActivity] Account Creation Feature Done

---
+++ app/src/main/java/com/example/app4a/presentation/main/CreateAccountActivity.kt
@@ -0,0 +1,67 @@
+package com.example.app4a.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import com.example.app4a.R
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
+import com.example.app4a.presentation.main.buttonStatus.SignInError
+import com.example.app4a.presentation.main.buttonStatus.SignInSuccess
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.activity_create_account.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    private val mainViewModel : MainViewModel by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+        mainViewModel.signInLiveData.observe(this, Observer {
+            when(it){
+                is SignInSuccess -> {
+                    MaterialAlertDialogBuilder(this)
+                        .setTitle("Account Created")
+                        .setMessage("Your Account Has Been Created, Please Login")
+                        .setPositiveButton("Ok") { dialog, which ->
+                            dialog.dismiss()
+                        }
+                        .show()
+                }
+                SignInError -> {
+                    si_mail_error.isVisible = true
+                    si_pw_error.isVisible = true
+                }
+            }
+        })
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        sign_in_button.setOnClickListener{
+            mainViewModel.onClickedSignIn(Email_CAA_edit.text.toString().trim(),Password_CAA_edit.text.toString().trim())
+        }
+
+
+        Email_CAA_edit.addTextChangedListener(SignInTextWatcher);
+        Password_CAA_edit.addTextChangedListener(SignInTextWatcher);
+    }
+
+    private val SignInTextWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            sign_in_button.isEnabled = !Email_CAA_edit.text.toString().trim().isEmpty() || !Password_CAA_edit.text.toString().trim().isEmpty()
+        }
+    };
+}
--- app/src/main/java/com/example/app4a/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/app4a/presentation/main/MainViewModel.kt
@@ -5,22 +5,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.app4a.domain.entities.User
 import com.example.app4a.domain.useCases.CreateUserUseCase
+import com.example.app4a.domain.useCases.GetAllUserUseCase
 import com.example.app4a.domain.useCases.GetUserUseCase
+import com.example.app4a.presentation.main.buttonStatus.*
+import kotlinx.android.synthetic.main.activity_create_account.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainViewModel(
     private val createUUC : CreateUserUseCase,
-    private val getUserUseCase: GetUserUseCase
+    private val getUserUseCase: GetUserUseCase,
+    private val getAllUserUseCase: GetAllUserUseCase
 ) : ViewModel() {
 
-    //val conditionPassword : MutableLiveData<String> = MutableLiveData();
     val loginLiveData : MutableLiveData<LoginStatus> = MutableLiveData();
-
- //   init {
-   //     conditionPassword.value = ""
-    //}
+    val signInLiveData : MutableLiveData<SignInStatus> = MutableLiveData();
 
     fun onClickedLogin(emailUser: String, password: String) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -33,6 +33,31 @@ class MainViewModel(
 
             withContext(Dispatchers.Main) {
                 loginLiveData.value = loginStatus
+            }
+        }
+    }
+
+    // Check Correct user entry and create new user in db if everything is ok
+    fun onClickedSignIn(emailUser: String, password: String) {
+        val regex0 = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}\$".toRegex()
+        val regex = "[a-zA-Z0-9]{1,16}@[a-zA-Z0-9]{4,8}.[a-zA-Z]{2,3}".toRegex()
+        val user: User = User(emailUser, password)
+        if (regex.matches(emailUser) && regex0.matches(password)) {
+            viewModelScope.launch(Dispatchers.IO) {
+                createUUC.invoke(user)
+            }
+        }
+
+        viewModelScope.launch(Dispatchers.Default) {
+            val au = getAllUserUseCase.invoke(user)
+            val signInStatus: SignInStatus = if (regex.matches(emailUser) && regex0.matches(password)) {
+                SignInSuccess(emailUser,au)
+            } else {
+                SignInError
+            }
+
+            withContext(Dispatchers.Main) {
+                signInLiveData.value = signInStatus
             }
         }
     }
